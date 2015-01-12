ec4209c52df14c05ddf1750c3e60f85269d11bdb
LittleWhite <menghan1218@gmail.com>
2015-01-12 15:12:18 +0800

    修改删除相同文件名的bug

--- rm.sh
+++ rm.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 trash_dir="/home/zhangmenghan/.Trash"
 rm_files=`echo "$*" | tr ' ' '\n' | grep -v "^-" | tr '\n' ' '`
-if [ x$rm_files = x ];then
+if [ x"$rm_files" = x ];then
     echo "arg error!"
     exit 1
 fi
 date=`date +"%y-%m-%d_%H:%M:%S:%N"`
 mkdir -p $trash_dir/$date
-mv $rm_files $trash_dir/$date
+for file in $rm_files
+do
+    file_name=${file##*/}
+    mv -f $file $trash_dir/$date/$file_name.`date +%N`
+done
